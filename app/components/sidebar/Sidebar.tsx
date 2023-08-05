f15f41a3cec17c00d67dbf3a8fad4431bdefcdfd
Artur0705 <artlil420@gmail.com>
2023-08-05 22:18:37 +1000

    Created custom hooks for routes and conversation, created sidebar for desktop and mobile

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
 
 async function Sidebar({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full">
       <main className="lg:pl-20 h-full">
         <DesktopSidebar />
+        <MobileFooter />
         {children}
       </main>
     </div>
