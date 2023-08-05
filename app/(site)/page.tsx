eaeabfe43adf468b89efdb3147df565670f62770
Artur0705 <artlil420@gmail.com>
2023-08-05 17:43:31 +1000

    Created Auth form page, Input, Button components and styled them,defined variants for login and register therefore changing the UI for login and register

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
@@ -14,11 +15,7 @@ export default function Home() {
          bg-gray-100
       "
     >
-      <div
-        className="
-                sm:mx-auto sm:w-full  sm:max-w-md
-            "
-      >
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           alt="logo"
           height="48"
@@ -28,12 +25,18 @@ export default function Home() {
         />
         <h2
           className="
-                    mt-6 text-center text-3xl font-bold tracking-tight text-gray-900              
-                "
+             mt-6
+             text-center
+             text-3xl
+             font-bold
+             tracking-tight
+            text-gray-900              
+          "
         >
           Sign in to your Account
         </h2>
       </div>
+      <AuthForm />
     </div>
   );
 }
